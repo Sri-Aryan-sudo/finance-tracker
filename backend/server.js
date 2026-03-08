d37a0ae3114f9f1149d630f54d8e6544a57cecea
Sri-Aryan-sudo <aryanthecoder18@gmail.com>
2026-03-08 15:21:27 +0530

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
