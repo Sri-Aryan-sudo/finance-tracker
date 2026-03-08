4224710fdffd571fea84cfc9f96f18458b1b216f
Sri-Aryan-sudo <aryanthecoder18@gmail.com>
2026-03-08 15:42:35 +0530

    auth.js updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/authController');
 
-router.post('/api/signup', signup);
-router.post('/api/login', login);
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router;
