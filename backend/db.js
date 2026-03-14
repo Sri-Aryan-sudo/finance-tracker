6457e9f79f9316b41c5b8678fa336bb3f08857f6
Sri-Aryan-sudo <aryanthecoder18@gmail.com>
2026-03-14 15:07:58 +0530

    db.js updated

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
