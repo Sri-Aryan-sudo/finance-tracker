7f852c1898dbf0ac1cc6f65a9ef4e28279410d62
Sri-Aryan-sudo <aryanthecoder18@gmail.com>
2026-03-14 15:29:33 +0530

    db.js changed

--- backend/db.js
+++ backend/db.js
@@ -6,8 +6,14 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
-
-pool.on('connect', () => console.log('Connected to PostgreSQL'));
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error('Database connection failed:', err.message);
+  } else {
+    console.log('Database connected successfully!');
+    release();
+  }
+});
 pool.on('error', (err) => { console.error(err); process.exit(-1); });
 
 module.exports = pool;
